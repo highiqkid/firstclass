68074fe0af19d6824beea3246577c30c85cf8c4f
dannycho7 <dannycho7@gmail.com>
2016-09-18 14:01:53 -0700

    Update script.py

--- script.py
+++ script.py
@@ -11,3 +11,4 @@ driver.execute_script("document.getElementById('pageContent_passwordText').value
 driver.execute_script("document.getElementById('pageContent_loginButton').click();")
 driver.execute_script("document.getElementById('pageContent_ContinueButton').click();")
 driver.execute_script("document.getElementById('ctl07').click();")
+driver.execute_script("var selectedVal='ANTH'; var sel=document.getElementById('pageContent_subjectAreaDropDown'); for(var i=0;i<sel.length;i++){if(sel.options[i].value){var noSpaceSel=sel.options[i].value.replace(' ','');}if(noSpaceSel==selectedVal){sel.selectedIndex=i;break;}}");
