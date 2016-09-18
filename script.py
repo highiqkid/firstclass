782b96a059e7e66d9024351d3657290ccb7a0618
highiqkid <victor_cheng@yahoo.com>
2016-09-18 13:39:51 -0700

    login

--- script.py
+++ script.py
@@ -1,15 +1,13 @@
 from selenium import webdriver
-from os.path import join, dirname, abspath
 from selenium.webdriver.support.ui import Select
 from selenium import webdriver
 from selenium.webdriver.support import ui
 from selenium.webdriver.common.keys import Keys
-from random import randint
-import time
 
 driver = webdriver.Chrome()
 driver.get("https://my.sa.ucsb.edu/gold/login.aspx")
 driver.execute_script("document.getElementById('pageContent_userNameText').value = 'victorcheng';")
 driver.execute_script("document.getElementById('pageContent_passwordText').value = '';")
 driver.execute_script("document.getElementById('pageContent_loginButton').click();")
-
+driver.execute_script("document.getElementById('pageContent_ContinueButton').click();")
+driver.execute_script("document.getElementById('ctl07').click();")
